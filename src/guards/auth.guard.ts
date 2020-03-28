91419db7751a6ae241903bf56cad075c080eee4c
oussama_yousfi <oussama.yousfi@esprit.tn>
2020-03-28 19:04:47 +0100

    starter project

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor( private router: Router) {}
+  canActivate(): boolean {
+      return true ;
+    }  
+}
